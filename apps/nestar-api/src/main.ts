e9308955ba15d9682f09b5ce3c16c568e6987268
Kamron <kamronmirzooo@gmail.com>
2024-12-04 12:51:15 +0900

    fix: modify nest cli detail

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -4,11 +4,6 @@ import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-
-	// it had PayloadTooLargeError: request entity too large in POSTMAN
-	app.use(bodyParser.json({ limit: '50mb' }));
-	app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-
 	await app.listen(process.env.PORT_API ?? 3000);
 }
 bootstrap();
