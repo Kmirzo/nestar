c634cce2dbc6227fb5c8fc2970a680afaeac436e
Kamron <kamronmirzooo@gmail.com>
2024-12-06 13:59:25 +0900

    feat: creation and verification jwt

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -18,8 +18,8 @@ export class MemberService {
 		input.memberPassword = await this.authService.hashPassword(input.memberPassword);
 		try {
 			const result = await this.memberModel.create(input);
+			result.accessToken = await this.authService.createToken(result);
 
-			// TODO: Authentication via Token
 			return result;
 		} catch (err) {
 			console.log('Error, Service.model:', err.message);
@@ -39,10 +39,10 @@ export class MemberService {
 		} else if (response.memberStatus === MemberStatus.BLOCK)
 			throw new InternalServerErrorException(Message.BLOCKED_USER);
 
-		// TODO: Compare password
-
 		const isMatch = await this.authService.comparePassword(input.memberPassword, response.memberPassword);
 		if (!isMatch) throw new InternalServerErrorException(Message.WRONG_PASSWORD);
+		response.accessToken = await this.authService.createToken(response);
+
 		return response;
 	}
 
